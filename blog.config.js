41a3674bd045c00792042e904e563fe4f7b9b5cf
pbnasmedia <102514319+pbnasmedia@users.noreply.github.com>
2022-03-29 12:43:17 +0700

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -5,13 +5,13 @@ const BLOG = {
   link: 'https://pembebasan.id',
   description: 'Media PEMBEBASAN',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
-  appearance: 'auto', // ['light', 'dark', 'auto'],
+  appearance: 'light', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
-  since: 2021, // If leave this empty, current year will be used.
-  postsPerPage: 7,
+  since: 2010, // If leave this empty, current year will be used.
+  postsPerPage: 10,
   sortByDate: false,
   showAbout: true,
   showArchive: true,
