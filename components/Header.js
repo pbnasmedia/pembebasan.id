078120b8b26903dd5dd89fdbc8d556780e2c36a9
pbnasmedia <102514319+pbnasmedia@users.noreply.github.com>
2022-03-31 08:48:35 +0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -6,10 +6,11 @@ import { useLocale } from '@/lib/locale'
 const NavBar = () => {
   const locale = useLocale()
   const links = [
-    { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
-    { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
-    { id: 2, name: locale.NAV.RSS, to: '/feed', show: true },
-    { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true }
+    { id: 0, name: 'Artikel', to: BLOG.path || '/tag/Artikel', show: true },
+    { id: 1, name: 'Sikap', to: BLOG.path || '/tag/Sikap', show: true },
+    { id: 2, name: 'Kegiatan', to: BLOG.path || '/tag/Kegiatan', show: true },
+    { id: 3, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
+    { id: 4, name: locale.NAV.SEARCH, to: '/search', show: true }
   ]
   return (
     <div className="flex-shrink-0">
