5dc8404061b2aa9b7767ce50aed45eaf601b44d3
pbnasmedia <102514319+pbnasmedia@users.noreply.github.com>
2022-03-31 09:05:42 +0700

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,17 +1,17 @@
 const BLOG = {
-  title: 'PEMBEBASAN',
+  title: 'PEMBEBASAN.ORG',
   author: 'Pembebasan',
   email: '',
-  link: 'https://pembebasan.id',
-  description: 'Media PEMBEBASAN',
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
-  appearance: 'light', // ['light', 'dark', 'auto'],
+  link: 'https://pembebasan.org',
+  description: 'Situs resmi Pusat Perjuangan Mahasiswa untuk Pembebasan Nasional.',
+  lang: 'id-ID', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2010, // If leave this empty, current year will be used.
-  postsPerPage: 10,
+  postsPerPage: 15,
   sortByDate: false,
   showAbout: true,
   showArchive: true,
